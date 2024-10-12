358b645c3c9166613984cf664b463305fe683efb
Oleksandr Kamlykov <aleksandrkamlykov@gmail.com>
2024-10-12 15:59:32 +0300

    gallery

---
+++ 12-10-24/Models/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace _12_10_24.Models
+{
+    public class Image
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string FilePath { get; set; }
+    }
+}
---
+++ 12-10-24/ViewComponents/ImageGalleryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using _12_10_24.Models;
+
+namespace _12_10_24.ViewComponents
+{
+    public class ImageGalleryViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var images = new List<Image>
+            {
+                new Image { Title = "Surface Abstract 1", Description = "Description 1", FilePath = "/images/Surface Abstract_1.png" },
+                new Image { Title = "Surface Abstract 2", Description = "Description 2", FilePath = "/images/Surface Abstract_2.png" },
+                new Image { Title = "Surface Abstract 3", Description = "Description 3", FilePath = "/images/Surface Abstract_3.png" },
+                new Image { Title = "Surface Abstract 3", Description = "Description 3", FilePath = "/images/Surface Abstract_4.png" }
+
+            };
+
+            return View(images);
+        }
+    }
+}
